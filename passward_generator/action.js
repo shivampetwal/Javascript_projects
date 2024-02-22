c541cfb06018a304aa7c18e441fcdf40508d28b2
shivam petwal <petwalshivam@gmail.com>
2024-02-22 20:34:52 +0530

    initial  commit

---
+++ passward_generator/action.js
@@ -0,0 +1,38 @@
+function generatePassword(length, includeLowercase, includeUppercase, includeNumbers, includeSymbols) {
+    const lowercase = "abcdefghijklmnopqrstuvwxyz";
+    const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const numbers = "0123456789";
+    const symbols = "!@#$%^&*()_+[]{}|;:,.<>?";
+
+    let password = "";
+    let allowedChar = "";
+    allowedChar += includeLowercase ? lowercase : "";
+    allowedChar += includeUppercase ? uppercase : "";
+    allowedChar += includeNumbers ? numbers : "";
+    allowedChar += includeSymbols ? symbols : "";
+
+    if (length < 5) {
+        return "Password length must be at least 5.";
+    }
+
+    if (allowedChar.length === 0) {
+        return "Select at least one option.";
+    }
+
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * allowedChar.length);
+        password += allowedChar[randomIndex];
+    }
+
+    return password;
+}
+
+const length = 12;
+const includeLowercase = true;
+const includeUppercase = true;
+const includeNumbers = true;
+const includeSymbols = true;
+
+const password = generatePassword(length, includeLowercase, includeUppercase, includeNumbers, includeSymbols);
+
+console.log(`Password: ${password}`);
